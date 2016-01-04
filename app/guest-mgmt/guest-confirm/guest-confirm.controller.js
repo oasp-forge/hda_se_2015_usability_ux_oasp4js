eefc24af282261a9e8de25d4fa1a3a739932e459
David Konczewski <david.konczewski@giordano-online.de>
2016-01-04 17:38:53 +0100

    changed Designe and alert messages after Reservation

--- app/guest-mgmt/guest-confirm/guest-confirm.controller.js
+++ app/guest-mgmt/guest-confirm/guest-confirm.controller.js
@@ -7,8 +7,9 @@ angular.module('app.guest-mgmt')
         $scope.book = function () {
             var number = Math.floor( Math.random() * (99999 - 10000)+ 10000);
             var alert = {
-                msg: "Ihre Reservierungsnummer lautet: "+number+". Bitte notieren Sie sich diese. Bei Stornierungen oder Rückfragen rufen Sie uns an.",
-                header: "Reservierungs erfolgreich!",
+                msg: "Ihre Reservierungsnummer lautet: "+number+". Bitte notieren Sie sich diese.",
+                msgSecond: "Sollten Sie Fragen haben oder Ihre Reservierung stornieren wollen, rufen Sie uns an unter XXXXX-XXXX.",
+                header: "Reservierung erfolgreich!",
                 type:"success"
             }
             menu.setAlert(alert);
